77762b2a4ed2c24be37062fb19afa143e714f3e8
ewilson87 <32503240+ewilson87@users.noreply.github.com>
2017-10-05 21:33:18 -0700

    Add files via upload

---
+++ Week1ewilson/PrgmExercise1.2/PrgmExercise.java
@@ -0,0 +1,21 @@
+
+/**
+ * Program: PrgmExercise File: PrgmExercise.java Summary: Displays three lines
+ * of text including my name, favorite color and what I would do if I won the
+ * lottery. Author: Evan Wilson Date: 4 October 2017 This work is my own based
+ * on the instruction of Instructor Lisa Herbert
+ */
+public class PrgmExercise {
+//begin PrgmExercise class
+
+    public static void main(String[] args) {
+        //begin main method 
+        System.out.println("Hello, my name is Evan Wilson.");
+        System.out.println("My favorite color is blue.");
+        System.out.println("If I won the lottery I would end my enlistment in"
+                + " the Navy and just enjoy life with my family.");
+    }
+//end main method
+
+}
+//end PrgmExercise class
---
+++ Week1ewilson/PrgmExercise1.3/Introduction.java
@@ -0,0 +1,16 @@
+
+/**
+ * Program: My First Project File: MyFirstProject.java Summary: Follows
+ * PrgmExercise1.2 instructions in creating an Introduction class and a main
+ * method. Author: Evan Wilson Date: 5 October 2017 This work is my own based on
+ * the instruction of Instructor Lisa Herbert
+ */
+public class Introduction {
+//begin Ingtroduction class
+
+    public static void main(String[] args) {
+        //begin main method 
+    }   //end main method
+
+}
+//end Introduction class
